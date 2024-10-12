75c70adea29822a84a9942c49505a09bc0e38f6c
AlanGcDev <gamebrocoli@gmail.com>
2024-10-12 11:27:47 -0600

    s

--- .vercel/cache/manage/infomundo/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class InfomundoConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'infomundo'
--- .vercel/cache/manage/infomundo/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views.index import *
-
-
-urlpatterns = [
-    path("", home,  name="index")  
-]
